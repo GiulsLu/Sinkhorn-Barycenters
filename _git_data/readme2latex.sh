e1eb3b200efd3f9a34576a3e9bac1068a213e3ef
Carlo Ciliberto <cciliber@gmail.com>
2019-05-31 01:47:55 +0200

    still experimenting with latex2readme

--- _git_data/readme2latex.sh
+++ _git_data/readme2latex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m readme2tex --nocdn --pngtrick --output ./readme-latex-output.md --readme ./readme-latex.md --svgdir ./svgs
+python -m readme2tex --nocdn --rerender --valign --pngtrick --output ./readme-latex-output.md --readme ./readme-latex.md --svgdir ./svgs
